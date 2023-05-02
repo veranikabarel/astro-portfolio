cdfb24f63ded644e42d2de7ad91edfd109ee587b
Veranika Kasparevych <veranikabarel@gmail.com>
2023-05-02 18:45:50 +0200

    add responsive layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,12 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	 theme: {
 		screens: {
+			'xs': '480px',
       'sm': '640px',
+			// tablet: '768px',
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
-      '2xl': '1536px',
     },
     colors: {
       'terracotta': '#c24332',
