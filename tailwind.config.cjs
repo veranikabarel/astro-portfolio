a1f9b3d969bd18fed77fe88e8aec7b6a4bfe3458
Veranika Kasparevych <veranikabarel@gmail.com>
2023-05-02 18:01:58 +0200

    add basic theme config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
+	 theme: {
+		screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      'terracotta': '#c24332',
+			'navy': '#2e303f',
+			'green': '#63948c',
+			'white': '#ebe6d1',
+    },
+    fontFamily: {
+      sans: ['Karla', 'sans-serif'],
+      monospace: ['Inconsolata', 'monospace'],
+    },
+		fontSize:{
+			'xs': '.75rem',
+			'sm': '.875rem',
+			'tiny': '.875rem',
+			'base': '1rem',
+			'lg': '1.125rem',
+			'xl': '1.25rem',
+			'2xl': '1.5rem',
+			'3xl': '1.875rem',
+			'4xl': '2.25rem',
+			'5xl': '3rem',
+		},
+  },
 	plugins: [
 		require('@tailwindcss/forms'),
 	],
