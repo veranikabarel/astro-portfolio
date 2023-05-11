08e46bc6bb71285e1c5dd40a75dbe7c44549d962
Veranika Kasparevych <veranikabarel@gmail.com>
2023-05-11 18:05:52 +0200

    add dark and light themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	lightMode: 'class',
 	theme: {
 		screens: {
 			xs: '480px',
